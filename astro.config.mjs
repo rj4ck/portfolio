76dfa014d68eb660837435d4d5515df8f16a542e
Raquel Jackson <r.jackson@msn.com>
2024-07-12 17:18:08 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rj4ck.github.io',
+  site: 'https://rj4ck.github.io/portfolio',
   base: '/portfolio/',
   integrations: [
     react(),
