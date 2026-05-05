37e4f9b52bf7ea2a8168e9ee19358fae456eb2b7
Raquel Jackson <r.jackson@msn.com>
2026-05-05 13:35:26 -0600

    fix: remove vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,27 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-import vercel from "@astrojs/vercel/static";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  // Configuración para Vercel (sin base path)
-  site: import.meta.env.SITE || 'https://rj4ck-portfolio.vercel.app',
+  site: "https://rj4ck.github.io",
+  base: "/portfolio",
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en"],
     routing: {
       prefixDefaultLocale: false,
-      redirectToDefaultLocale: true
-    }
+      redirectToDefaultLocale: true,
+    },
   },
   output: "static",
-  adapter: vercel(),
   integrations: [
     react(),
     tailwind(),
     partytown({
       config: {
         forward: ["dataLayer.push"],
-      }
-    })
+      },
+    }),
   ],
-  // Asegurar que los scripts de Partytown se copien correctamente
-  vite: {
-    ssr: {
-      noExternal: ['@astrojs/partytown']
-    }
-  }
 });
